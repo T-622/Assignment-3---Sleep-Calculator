0189ae47b96180f9cecae1de55fc38ceb6f157eb
Tyler Peppy <61562476+T-622@users.noreply.github.com>
2022-10-27 14:42:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,317 @@
+# UTF-8 CRLF
+# Done in REPL.IT & Python 3.8.4 32-bit interpreter
+# Reproduction without citation is exclusively NOT permitted
+# Licensed under GNU GPLv3.0
+
+# Copyright (c) 2022 Tyler Peppy @ OCDSB
+
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+import time
+import math
+import numpy as np
+from datetime import datetime
+from datetime import date
+
+MAGENTA = "\u001b[35m"
+BLACK = "\033[0;30m"
+YELLOW = "\u001b[33m"
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+BROWN = "\033[0;33m"
+BLUE = "\033[0;34m"
+PURPLE = "\033[0;35m"
+CYAN = "\033[0;36m"
+LIGHT_GRAY = "\033[0;37m"
+DARK_GRAY = "\033[1;30m"
+LIGHT_RED = "\033[1;31m"
+LIGHT_GREEN = "\033[1;32m"
+YELLOW = "\033[1;33m"
+LIGHT_BLUE = "\033[1;34m"
+LIGHT_PURPLE = "\033[1;35m"
+LIGHT_CYAN = "\033[1;36m"
+LIGHT_WHITE = "\033[1;37m"
+RESET = "\u001b[0m"
+BOLD = "\033[1m"
+FAINT = "\033[2m"
+ITALIC = "\033[3m"
+UNDERLINE = "\033[4m"
+BLINK = "\033[5m"
+NEGATIVE = "\033[7m"
+CROSSED = "\033[9m"
+
+name = ""
+
+current = [0,0,0] # Current Date As Array Instead Of 3 Vars
+birth = [0,0,0]   # Birth Date As Array Instead Of 3 Vars
+
+daysOld = 0
+numLeapYears = 0
+d1 = 0
+d2 = 0
+weeklyHours = 0 # Hours Slept During Week
+endlyHours = 0  # Hours Slept During Weekend
+
+weekDays = 0    # Number Of Weekdays
+endDays = 0     # Number Of Weekends
+
+totalHours = 0
+
+totalWeek = 0
+totalEnd = 0
+
+ageBreakdown = [0,0,0,0,0,0] # Array to Store YY/MM/DD/HH/MM/SS
+
+today = date.today() # Define Current Date
+
+def playAgain():
+  while True:
+    try:
+      choice = input(BLUE+"\nWould You Like To Play Again? 'y' To Continue, And 'n' To Exit: "+RESET)
+    except ValueError:
+      print(RED+"\nInvalid Choice!"+RESET)
+    else:
+      if (choice in ""):
+        print(RED+"\nInvalid Choice!"+RESET)
+      elif(choice not in "yYnN"):
+        print(RED+"\nInvalid Choice!"+RESET)
+      elif (choice in "yY"):
+        print("")
+        return True
+      elif (choice in "nN"):
+        print(GREEN+"\nThanks For Playing!"+RESET)
+        return False
+
+def getBirthDate():
+  global birth
+
+  while True:
+    try:
+      birth[0] = int(input(BLUE+"What's Your Birth Year (YY): "+RESET))
+    except ValueError:
+      print(RED+"\nYou Didn't Enter A Valid Month!"+RESET) 
+    if (birth[0] <= 0):
+      print(RED+"\nToo Low Year"+RESET)
+    elif (birth[0] > 2100):
+      print(RED+"\nYear too Large"+RESET)
+    else:
+      break
+  
+  while True:
+    try:
+      birth[2] = int(input(BLUE+"What's Your Birth Month (MM): "+RESET))
+    except ValueError:
+      print(RED+"\nYou Didn't Enter A Valid Month!"+RED) 
+    if (birth[2] > 12):
+      print(RED+"\nThere Aren't More Than 31 Days In A Month!"+RESET)
+    elif (birth[2] <= 0):
+      print(RED+"\nValue Too Small!"+RESET)
+    else:
+      break
+
+  while True:
+    try:
+      birth[1] = int(input(BLUE+"\nWhat's Your Birthday (DD): "+RESET))
+    except ValueError:
+      print(RED+"\nYou Didn't Enter A Valid Day!"+RESET) 
+    if (birth[1] > 31):
+      print(RED+"\nThere Aren't More Than 31 Days In A Month!"+RESET)
+    elif (birth[1] <= 0):
+      print(RED+"\nValue Too Small!"+RESET)
+    else:
+      if(birth[0]%4 != 0):                                    # If Not A Leap Year
+        if(birth[2] in [1,3,5,7,8,10,12] and birth[1] > 31):  # If A 31 Day-Month, Don't Accecpt Larger Than 31 Days
+          print(RED+"\nBad Day!"+RESET)
+        elif (birth[2] in [4,6,9,11] and birth[1] > 30):      # If A 30 Day-Month, Don't Accecpt Larger Than 30 Days
+          print(RED+"\nBad Day!"+RESET)
+        elif (birth[2] == 2 and birth[1] > 28):               # If A 28 Day Month (Non-Leap), Don't Accecpt Larger Than 28 Days
+          print(RED+"\nBad Day!"+RESET)
+        else:
+          break
+      
+      elif(birth[0]%4 == 0 and birth[0]%100 != 0) or (birth[0]%400 == 0):  # If A Leap Year
+        if(birth[2] in [1,3,5,7,8,10,12] and birth[1] > 31):               # If A 31-Day Month, Don't Accecpt Larger Than 31 Days
+          print(RED+"\nBad Day!"+RESET)
+        elif (birth[2] in [4,6,9,11] and birth[1] > 30):                   # If A 30-Day Month, Don't Accecpt Larger Than 30 Days
+          print(RED+"\nBad Day!"+RESET)
+        elif (birth[2] == 2 and birth[1] > 29):                            # If A 29-Day Month, Don't Accecpt Larger Than 29 Days
+          print(RED+"\nBad Day!"+RESET)
+        else:
+          break
+    
+  print(MAGENTA+"\nEntered Date! (DD/MM/YYYY):",birth[1],"/",birth[2],"/",birth[0],""+RESET)
+
+def getCurrentDate():
+  global current
+  while True: 
+    try:
+      currentDate = input(BLUE+"\nUse Current Date? 'y' To Continue, Or 'n' To Manually Enter Date: "+RESET)
+    except ValueError:
+      print(RED+"\nI Need A Definitive Answer!"+RESET)
+    else:
+      if (currentDate in ""):
+        print(RED+"\nI Need A Definitive Answer!"+RESET)
+      elif (currentDate not in "yYnY"):
+        print(RED+"\nInvalid Answer"+RESET)
+      elif (currentDate in "yY"):
+        current[0] = today.year
+        current[1] = today.day
+        current[2] = today.month
+        break
+      else:
+        while True:
+          try:
+            current[1] = int(input(BLUE+"\nWhat's Today's Day (DD): "+RESET))
+          except ValueError:
+            print(RED+"\nYou Didn't Enter A Valid Day!"+RESET) 
+          if (current[1] > 31):
+            print(RED+"\nThere Aren't More Than 31 Days In A Month!"+RESET)
+          elif (current[1] <= 0):
+            print(RED+"\nValue Too Small!"+RESET)
+          else:
+            break
+      
+        while True:
+          try:
+            current[2] = int(input(BLUE+"What's Today's Month (MM): "+RESET))
+          except ValueError:
+            print(RED+"\nYou Didn't Enter A Valid Month!"+RESET) 
+          if (current[2] > 12):
+            print(RED+"\nThere Aren't More Than 12 Months In A Year"+RESET)
+          elif (current[2] <= 0):
+            print(RED+"\nValue Too Small!"+RESET)
+          else:
+            break
+      
+        while True:
+          try:
+            current[0] = int(input(BLUE+"What's Today's Year (YY): "+RESET))
+          except ValueError:
+            print(RED+"\nYou Didn't Enter A Valid Month!"+RESET) 
+          if (current[0] <= 0):
+            print(RED+"\nCan't Have Year 0 Or Less"+RESET)
+          elif (current[0] < birth[0]):
+            print(RED+"\nCan't have A Year Smaller Than Your Birth Year!"+RESET)
+          else:
+            break
+  print(MAGENTA+"\nCurrent Date! (DD/MM/YYYY):",current[1],"/",current[2],"/",current[0])
+
+def getDiff(date1, date2):
+  delta = date1 - date2
+  return delta.days
+def getInformation():
+  global birth
+  global current
+  global name
+  while True:
+    try:
+      name = input(BLUE+"What's Your Name?: "+RESET)
+    except ValueError:
+      print("You Didn't Enter A Valid Name!")
+    else:
+      print(YELLOW+"\nHello "+name+", Ready To Calculate Your Dormant Age?"+RESET)
+      break
+
+  print(GREEN+"\nTime To Get Your Birthdate!"+RESET)
+  getBirthDate()
+  print(GREEN+"\nTime To Get The Current Date!"+RESET)
+  getCurrentDate()
+
+def breakdown(totalHours):
+  ageBreakdown[0] = totalHours/8760 # Convert TotalHours To Years
+  
+  result = math.modf(ageBreakdown[0])  # Split Decimal From Years Calculation And Get Months
+  ageBreakdown[1] = result[0]*12
+  
+  result = math.modf(ageBreakdown[1]) # Split Months From Decimal And Convert To Days
+  ageBreakdown[2] = result[0]*30.417
+
+  result = math.modf(ageBreakdown[2]) # Split Months From Decimal And Convert To Days
+  ageBreakdown[3] = result[0]*24
+
+  result = math.modf(ageBreakdown[3])
+  ageBreakdown[4] = result[0]*60
+
+  result = math.modf(ageBreakdown[4])
+  ageBreakdown[5] = result[0]*60
+  
+  print(GREEN+"\nYour Sleep Statistics:"+RESET)
+  print(YELLOW+"Years:",int(ageBreakdown[0]))
+  print("Months:",int(ageBreakdown[1]))
+  print("Days:",int(ageBreakdown[2]))
+  print("Hours:",int(ageBreakdown[3]))
+  print("Minutes:",int(ageBreakdown[4]))
+  print("Seconds:",ageBreakdown[5])
+  
+
+while True:
+  getInformation()
+  d1 = date(current[0], current[2], current[1]) # YY/MM/DD
+  d0 = date(birth[0], birth[2], birth[1])       # YY/MM/DD
+  daysOld = getDiff(d1, d0)
+  
+  for x in range (birth[0], current[0]):
+    if(x%4 == 0 and x%100 != 0) or (x%400 == 0): 
+      daysOld += 1  
+      numLeapYears += 1
+  print(YELLOW+"\nYou Are:",daysOld,"Days Old! (Including)",numLeapYears,"Leap Years"+RESET)
+
+  while True:
+    try:
+      weeklyHours = int(input(BLUE+"\nHow Many Hours Do You Average Sleeping Per WEEK NIGHT: "+RESET))
+    except ValueError:
+      print(RED+"\nValues Aren't Optional!"+RESET)
+    else:
+      if(weeklyHours > 24): 
+        print(RED+"\nYou Can't Have More Than 24H Of Sleep In A Day..."+RESET)
+      elif(weeklyHours <= 0):
+        print(RED+"\nYou Can't Have Less Than 0H Of Sleep In A Day..."+RESET)
+      else:
+        break
+        
+  while True:
+    try:
+      endlyHours = int(input(BLUE+"How Many Hours Do You Average Sleeping Per WEEKEND: "+RESET))
+    except ValueError:
+      print(RED+"\nValues Aren't Optional!"+RESET)
+    else:
+      if(endlyHours > 24): 
+        print(RED+"\nYou Can't Have More Than 24H Of Sleep In A Day..."+RESET)
+      elif(endlyHours <= 0):
+        print(RED+"\nYou Can't Have Less Than 0H Of Sleep In A Day..."+RESET)
+      else:
+        break  
+
+  weekDays = np.busday_count(d0, d1)
+  endDays = daysOld - weekDays
+
+  totalWeek = weekDays*weeklyHours
+  totalEnd = endDays*endlyHours
+
+  print(MAGENTA+"\nYou Have Spent",totalWeek+totalEnd,"Hours Sleeping!"+RESET)
+  breakdown(totalWeek+totalEnd)
+
+  if(playAgain() == False):
+    break
+  else:
+    print("")
+  
